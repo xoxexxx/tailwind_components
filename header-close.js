b7d5a13d1c5c696d3c4ecdbd2af59c4656480bdc
ya <104529495+xoxexxx@users.noreply.github.com>
2024-07-08 08:47:02 +0300

    Update header-close.js

--- header-close.js
+++ header-close.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const HCloseBtn = () => {
+export const HCloseBtn = ({fill}) => {
 
   return  <div className="flex justify-between w-full">
         <span></span>
-        <svg class="h-6 w-6 cursor-pointer translate-x-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+        <svg class="h-6 w-6 cursor-pointer translate-x-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke={fill} aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
     </div>
